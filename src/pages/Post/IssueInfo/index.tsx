736e3abcc2d9814460775c0967462bb681c4bb41
Guilherme David <guilhermedavidrk@icloud.com>
2022-10-30 23:40:16 -0300

    refactor: improved code writing :memo:

--- src/pages/Post/IssueInfo/index.tsx
+++ src/pages/Post/IssueInfo/index.tsx
@@ -14,34 +14,32 @@ import { useContext } from 'react'
 
 export function IssueInfo({ issuesInfo }: ContentProps) {
   const { formatterDate } = useContext(BlogContext)
-  const element = <FontAwesomeIcon icon={faArrowUpRightFromSquare} />
-  const gitHub = <FontAwesomeIcon icon={faGithub} />
-  const calendar = <FontAwesomeIcon icon={faCalendarDay} />
-  const user = <FontAwesomeIcon icon={faComment} />
-  const chevronLeft = <FontAwesomeIcon icon={faChevronLeft} />
 
   return (
     <IssueContainer>
       <IssueContent>
         <header>
-          <NavLink to="/">{chevronLeft}voltar</NavLink>
+          <NavLink to="/">
+            {' '}
+            <FontAwesomeIcon icon={faChevronLeft} /> voltar
+          </NavLink>
           <a href={issuesInfo.html_url}>
             ver no github
-            {element}
+            <FontAwesomeIcon icon={faArrowUpRightFromSquare} />
           </a>
         </header>
         <h2>{issuesInfo.title}</h2>
         <footer>
           <span>
-            {gitHub}
+            <FontAwesomeIcon icon={faGithub} />
             {issuesInfo.user?.login}
           </span>
           <span>
-            {calendar}
+            <FontAwesomeIcon icon={faCalendarDay} />
             {`Há ${formatterDate(issuesInfo.created_at)}`}
           </span>
           <span>
-            {user}
+            <FontAwesomeIcon icon={faComment} />
             <strong>{issuesInfo.comments}</strong> comentários
           </span>
         </footer>
